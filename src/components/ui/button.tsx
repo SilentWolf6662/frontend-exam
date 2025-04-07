c89f24a1238856e852bfbb6ef0b634788c5c6f4e
SilentWolf6662 <52424766+SilentWolf6662@users.noreply.github.com>
2025-04-07 17:23:25 +0200

    Create button.tsx

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,62 @@
+import { cn } from "@/lib/utils";
+import type { ButtonHTMLAttributes } from "react";
+
+// Define variant styles
+const variantClasses = {
+    default: "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+    destructive:
+        "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+    outline:
+        "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+    secondary: "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+    ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+    link: "text-primary underline-offset-4 hover:underline",
+};
+
+type ButtonVariants = keyof typeof variantClasses
+
+// Define size styles
+const sizeClasses = {
+    default: "h-9 px-4 py-2 has-[>svg]:px-3",
+    sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+    lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+    icon: "size-9",
+};
+
+type ButtonSizeVariants = keyof typeof sizeClasses
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: ButtonVariants;
+    size?: ButtonSizeVariants;
+    asChild?: boolean;
+    className?: string;
+}
+
+function buttonVariants({ variant = "default", size = "default", className = "" }: Partial<ButtonProps>) {
+    return cn(
+        "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive cursor-pointer",
+        variantClasses[variant],
+        sizeClasses[size],
+        className
+    );
+}
+
+function Button({
+    className,
+    variant = "default", // Default variant
+    size = "default", // Default size
+    asChild = false,
+    ...props
+}: ButtonProps) {
+    // Determine the correct component (button or span) based on `asChild`
+    const Comp = asChild ? "span" : "button";
+
+    return (
+        <Comp
+            className={buttonVariants({ variant, size, className })}
+            {...props}
+        />
+    );
+}
+
+export { Button, buttonVariants };
